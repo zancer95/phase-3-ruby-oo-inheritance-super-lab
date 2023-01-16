2fe82ebdceb342aa556f2ecbfbe8bcede4dd3e09
ZhiLeong <saber_zhileong@hotmail.com>
2023-01-16 15:20:36 -0500

    commit tree 5b9833b2896309b40e7c9c879a40d81396a9f554

---
+++ lib/chatty_student.rb
@@ -0,0 +1,18 @@
+class ChattyStudent < Student
+
+
+
+    def hello
+        super
+        puts "How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died..."
+    end
+
+    def raise_hand
+        counter = 0
+        loop do 
+        counter += 1
+        super
+        break if counter == 10
+        end
+    end
+end
